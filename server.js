5500298600f5b2bfbe93e3191793c231b1015714
Christine Ting <christ123092@yahoo.com>
2020-02-07 12:16:27 -0800

    can view two modules on localhost:8000

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('div'));
+app.use(cors());
+
+
+
+app.use(express.static(path.join(__dirname, './')))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
